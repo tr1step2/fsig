ac492ecc9822fd029772e19f14836893a013aae5
A. Nesterkin <ul.morhekil@gmail.com>
2017-04-28 22:36:32 +0400

    Uncomment hasher

--- source/Hasher.cpp
+++ source/Hasher.cpp
@@ -4,10 +4,8 @@
 
 std::string fsig::Hasher::process_data(const std::string & data)
 {
-    /*boost::crc_32_type result;
+    boost::crc_32_type result;
     result.process_bytes(data.data(), data.length());
 
     return std::to_string(result.checksum());
-	*/
-	return data;
 }
